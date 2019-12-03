e567295ab76bca1a1b1b10e4677f4b351ee3a2a0
siccolo <siccolo@users.noreply.github.com>
2019-12-03 13:52:44 -0500

    Update output-prime-numbers.sql

--- output-prime-numbers.sql
+++ output-prime-numbers.sql
@@ -18,10 +18,8 @@ OPTION (MAXRECURSION 0)
 )
 SELECT STUFF(
 	( 
-		SELECT STRING_AGG( CAST(n AS VARCHAR(5)), '&') FROM NumberList nl
+		SELECT ',' + CAST(n AS VARCHAR(5)) FROM NumberList nl
 		WHERE NOT EXISTS (SELECT 1 FROM NumberList nl2 where nl2.n < nl.n AND nl.n % nl2.n = 0)
-	FOR XML PATH(''),TYPE
-	).value('text()[1]','nvarchar(max)'),1,2,N'')
+	 FOR XML PATH('')
+	), 1, 1, '')
 OPTION (MAXRECURSION 0)
-
-
