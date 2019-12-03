0c26f104b81d100878461620a7715b2f6065cbdb
siccolo <siccolo@users.noreply.github.com>
2019-12-03 13:20:37 -0500

    Create Binary-Tree-Node-CTE.sql

---
+++ Binary-Tree-Node-CTE.sql
@@ -0,0 +1,27 @@
+CREATE TABLE OO (N INT NULL, P INT NULL)
+GO
+INSERT INTO OO
+SELECT 1 , 2 
+UNION ALL 
+SELECT 3 , 2 
+UNION ALL 
+SELECT 6 , 8
+UNION ALL  
+SELECT 9 , 8
+UNION ALL  
+SELECT 2 , 5
+UNION ALL  
+SELECT 8 , 5
+UNION ALL  
+SELECT 5 , NULL
+GO
+
+;WITH h (v, vp, vlevel, vpath) AS (
+	SELECT root.N, NULL, 'root', CAST (root.N AS VARCHAR(MAX))FROM OO as root WHERE P IS NULL
+	UNION ALL 
+		SELECT leaf.N, leaf.P, 'leaf', h.vpath + ' -> ' + CAST (leaf.N AS VARCHAR(MAX))
+		FROM OO as leaf 
+			INNER JOIN h ON leaf.P = h.v
+)
+SELECT * FROM h
+ORDER BY v
